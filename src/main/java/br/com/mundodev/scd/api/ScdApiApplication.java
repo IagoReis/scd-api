9fb689f09678aed2ce2f4efb92e07def82752397
Iago Reis <iagoidrs@gmail.com>
2020-05-21 12:51:56 -0300

    Ah shit, here we go again

---
+++ src/main/java/br/com/mundodev/scd/api/ScdApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.mundodev.scd.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScdApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScdApiApplication.class, args);
+	}
+
+}
